3973679f0df4565edd474284988ac87083f38e25
Andrew-Atwater <andrew.atwater@maine.edu>
2025-04-06 18:51:05 -0400

    completed quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,16 @@
 class quicksort:
-    def find_piv(array):
-        if len(array) < 2:
-            return array
-        pivot = array[0]
+    def sort(array):
+        def quicksort(array):
+            if len(array) < 2:
+                return array
+            pivot = array[len(array)//2]
+            left = [x for x in array if x < pivot]
+            middle = [y for y in array if y == pivot]
+            right = [z for z in array if z > pivot]
+            return quicksort(left) + quicksort(middle) + quicksort(right)
+        return quicksort(array)
+    
+    def vf(original, sorted):
+        if sorted(original) and len(original) == len(sorted):
+            return(sorted)
+     
