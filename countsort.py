224509fd551e1495029cfe1e2dc8938faad4419b
Andrew-Atwater <andrew.atwater@maine.edu>
2025-04-06 18:57:17 -0400

    countsort complete

--- countsort.py
+++ countsort.py
@@ -1,2 +1,16 @@
 class countsort:
-    def __init__(arr):
+    def countsort(array):
+        if len(array) < 2:
+            return array
+        max = max(array)
+        count = [0] * (max + 1)
+        for num in array:
+            count[num] += 1
+        sorted = []
+        for value, freq in enumerate(count):
+            sorted.extend([value] * freq)
+        return sorted
+    
+    def vf(original, sorted):
+        if sorted(original) and len(original) == len(sorted):
+            return(sorted)
