5b4ee138b860ea8c8ef755e3b010e7479c74553e
Andrew-Atwater <andrew.atwater@maine.edu>
2025-04-06 16:04:34 -0400

    Selection sort complete

--- selection.py
+++ selection.py
@@ -1,2 +1,10 @@
 class selection:
-    def __init__(arr):
+    def selection(array):
+        x = len(array)
+        for i in range(x - 1):
+            min = i
+            for j in range(i + 1, x):
+                if array[j] < array[min]:
+                    min = j
+        target = array.pop(min)
+        array.insert(i, target)
